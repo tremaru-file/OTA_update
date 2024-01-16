2f0dbc4fd8a5fcbe13c04e004cac6d16cd07652e
drondistortion <cancelbutton@gmail.com>
2024-01-16 19:31:05 +0300

    comments

--- OTA_update.ino
+++ OTA_update.ino
@@ -3,17 +3,20 @@
 #include <Update.h>
 #include <LiquidCrystal_I2C.h> // 1.1.4
 #include <Button2.h> // 2.2.4
-#include "password.h"
 
 // данные для подключения
-//const char* ssid = "имя точки доступа";
-//const char* password = "пароль точки доступа";
+const char* ssid = "имя точки доступа";
+const char* password = "пароль точки доступа";
 
-const char* server = "https://tremaru-file.github.io/OTA_update/";
+// адрес сервера 
+const char* server = "адрес сервера для хранения файлов (слеш в конце строки обязателен)/";
+// для проверки можно использовать наш репозиторий с уже загруженными прошивками:
+//const char* server = "https://tremaru-file.github.io/OTA_update/";
 
-#define FW_LED LED_BUILTIN
-//#define FW_LED 18
-//#define FW_LED 32
+// выводы светодиодов (разкомментировать только один для каждой версии прошивки)
+#define FW_LED LED_BUILTIN // BLUE
+//#define FW_LED 18 // GREEN
+//#define FW_LED 32 // RED
 
 // макрос возможных прошивок (ИНДЕКС, СТРОКА ДЛЯ ЭКРАНА, НАЗВАНИЕ ФАЙЛА НА СЕРВЕРЕ)
 #define MENU\
@@ -26,7 +29,7 @@ constexpr unsigned BUTTON_LEFT = 13;
 // вывод правой кнопки
 constexpr unsigned BUTTON_RIGHT = 33;
 
-// состояние меню
+// состояние меню (cм. макрос MENU)
 typedef enum {
 #define X(INDEX, STR, FILENAME) INDEX,
 	MENU
@@ -34,6 +37,7 @@ typedef enum {
 	NMENUSTATES
 } state_t;
 
+// переопределение оператора приращивания для смены текущего состояния меню
 state_t& operator++(state_t& s)
 {
 	switch (s) {
@@ -46,13 +50,14 @@ state_t& operator++(state_t& s)
 
 state_t menu_state = FW1;
 
-// строки для вывода на дисплей
+// строки для вывода на дисплей (см. макрос MENU)
 const char* disp_strings[] {
 #define X(INDEX, STR, FILENAME) STR,
 	MENU
 #undef X
 };
 
+// строки названия файлов (см. макрос MENU)
 const char* filename_strings[] {
 #define X(INDEX, STR, FILENAME) FILENAME,
 	MENU
@@ -158,11 +163,14 @@ void released(Button2& btn)
 	}
 }
 
+// обновление прошивки
 void update()
 {
+	// запрос на сервер
 	HTTPClient client;
 	client.begin((String)server + filename_strings[menu_state]);
 
+	// если запрос удачный - обновляем раздел новым кодом
 	int httpcode = client.GET();
 	if (httpcode == HTTP_CODE_OK) {
 		disp.clear();
@@ -173,22 +181,33 @@ void update()
 		disp.clear();
 		disp.printf("HTTP: %d", httpcode);
 		Serial.printf("HTTP: %d", httpcode);
+		return;
 	}
+
 	client.end();
+
+	// перезагрузка
 	ESP.restart();
 }
 
 int g_full_length = 0;
 int g_curr_length = 0;
 
+// обновление раздела OTA
 void updateFirmware(HTTPClient& client) {
+	// хранение части полученных данных
 	uint8_t buff[128]{};
+	// получение информации о размере файла
 	g_full_length = client.getSize();
+	// размер для декрементации в цикле
 	int len = g_full_length;
+	// начинаем обновление
 	Update.begin(UPDATE_SIZE_UNKNOWN);
+	// получаем указатель на поток данных
 	WiFiClient* stream = client.getStreamPtr();
 
 	while (client.connected() && (len > 0 || len == -1)) {
+		// пока данные есть - записываем поэтапно в Flash память
 		size_t size = stream->available();
 		if (size) {
 			int bytes_read = stream->readBytes(buff, ((size > sizeof(buff))?sizeof(buff):size));
@@ -201,18 +220,24 @@ void updateFirmware(HTTPClient& client) {
 }
 
 
+// запись из RAM в Flash (параметры: указатель на массив в RAM, длинна массива)
 void updateFlash(uint8_t* data, size_t len)
 {
+	// Запись массива в Flash
 	Update.write(data, len);
+	// Обновление счётчика записсаного размера
 	g_curr_length += len;
 
+	// Вывод на дисплей точек на дисплей
 	static int count = 0;
 	count++;
 	if (count % 1024 == 0)
 		disp.print(".");
 
+	// ранний выход из функции, если записаны не все данные
 	if (g_curr_length != g_full_length)
 		return;
 
+	// завершение обновления
 	Update.end(true);
 }
